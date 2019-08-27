a9301304c95bd689e61377fa14b3da974f1dfe5b
Lokres <aleksvalmix@yandex.ru>
2019-08-27 17:41:06 +0300

    ignore changes

--- config/web.php
+++ config/web.php
@@ -65,33 +65,6 @@ $config = [
     ],
     'params' => $params,
     'modules' => [
-	'imagemanager' => [
-		'class' => 'noam148\imagemanager\Module',
-            /*    'as access' => [
-                        'class' => 'yii\filters\AccessControl',
-                        'rules' => [
-                            [
-                                'roles' => ['@'],
-                                'allow' => true,
-                                'matchCallback' => function ($rule, $action) {
-                                    return Yii::$app->user->identity->type > 5;
-                                }                   
-                            ],                   
-                            // И так далее
-                        ]
-                    ],*/
-		'canUploadImage' => true,
-		'canRemoveImage' => function(){
-                    return true;
-		},
-		'deleteOriginalAfterEdit' => false, // false: keep original image after edit. true: delete original image after edit
-		// Set if blameable behavior is used, if it is, callable function can also be used
-		'setBlameableBehavior' => false,
-		//add css files (to use in media manage selector iframe)
-		'cssFiles' => [
-			'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css',
-		],
-	],
     ],
     
 ];
